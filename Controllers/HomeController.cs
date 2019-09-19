adec2ec90faf85f3d4ae69eacaec54f60797e9b8
vijay113 <vijay113kumar@gmail.com>
2019-09-19 15:47:23 +0530

    init repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLogSample.Models;
+
+namespace NLogSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+        public IActionResult Index()
+        {
+            try {
+                logger.LogInformation("HomeController.Index method called!!!");
+                return View();
+            }
+            catch (Exception ex) {
+                logger.LogError(ex,"Error found in Index method");
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            try {
+               logger.LogWarning(string.Format("warning before use of Privacy"));
+               return View();
+            }
+            catch (Exception ex) {
+                return View();
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            logger.LogError("error found in HomeController.");
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace NLogSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main function");
+                CreateWebHostBuilder(args).Build().Run();
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Error in init");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+            // CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>().ConfigureLogging(logging =>
+               {
+                   logging.ClearProviders();
+                   logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+               })
+           .UseNLog();
+    }
+}
